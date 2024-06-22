1cdee4f433cd665b1b7ff76d33969413a7e2e21e
Aditya Gupta <113687793+Adityagupta9@users.noreply.github.com>
2024-06-22 11:57:00 +0530

    done ui

--- js/client.js
+++ js/client.js
@@ -1,5 +1,4 @@
-// const socket = io('https://localhost:8000');
-const socket = io("https://accessible-bubble-guavaberry.glitch.me/")
+const socket = io("https://accessible-bubble-guavaberry.glitch.me/");
 
 const form = document.getElementById('send-form');
 const messageInput = document.getElementById('message-input');
@@ -11,49 +10,71 @@ if (userName) {
     socket.emit('new-user-joined', userName);
 }
 
-var audio = new Audio('send.mp3');
-
-const append = (message, position) => {
+var audio = new Audio('recive.mp3');
+var audio2 = new Audio('log.mp4');
+var audio3 = new Audio('send.mp3')
+ 
+const append = (name, message, position) => {
+    const messageWrapper = document.createElement('div');
     const messageElement = document.createElement('div');
-    
-    messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    if (position === "left"){
-        const profilepic = document.createElement('img')
-        profilepic.src=("./profilepic.jpg")
+    const messageContent = document.createElement('p');
+    const messageName = document.createElement('span');
+
+    messageName.innerText = name;
+    messageName.classList.add('message-name');
+
+    messageContent.innerText = message;
+    messageContent.classList.add('message-content');
+
+    messageElement.classList.add('message', position);
+    messageElement.append(messageName);
+    messageElement.append(messageContent);
+
+    messageWrapper.classList.add('message-wrapper', position);
+    if (position === "left") {
+        const profilepic = document.createElement('img');
+        profilepic.src = "./profilepic.jpg";
         profilepic.classList.add("profilepic");
-        messageContainer.append(profilepic)
-        }
-    messageContainer.append(messageElement);
-    
-    if (position === "left" || position === "center") {
+        messageWrapper.append(profilepic);
+    }
+    messageWrapper.append(messageElement);
+
+    messageContainer.append(messageWrapper);
+
+    // Scroll to the latest message
+    messageWrapper.scrollIntoView({ behavior: 'smooth' });
+
+    if (position === "left" ) {
         audio.play();
     }
-
-    
+    if(position=="center"){
+        audio2.play();
+    }
+    if(position=="right"){
+        audio3.play();
+    }
 };
 
 // Listen for user joined
 socket.on('user-joined', name => {
-    append(`${name} has joined`, 'center');
+    append(`${name}`, ` has joined`, 'center');
 });
 
 // Listen for messages
 socket.on('received-message', data => {
-    append(`${data.name}: ${data.message}`, "left");
+    append(`~${data.name}`, data.message, "left");
 });
 
 // Listen for users leaving
 socket.on('left', name => {
-    append(`${name} left the chat`, "center");
+    append(`${name}`, ` left the chat`, 'center');
 });
 
 // Send message
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, "right");
+    append('~You', message, "right");
     socket.emit('send-message', message);
     messageInput.value = '';
 });
